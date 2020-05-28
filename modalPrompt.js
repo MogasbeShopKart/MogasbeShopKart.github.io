f062ee0f6bc7e15d84b6f1b1754c5a10a67b7a09
MogasbeKelani <MogasbeKelani@gmail.com>
2020-05-27 20:47:06 -0700

    modals pushed

---
+++ modalPrompt.js
@@ -0,0 +1,11 @@
+
+var modal="";
+
+function displayModal(id) {
+  modal = document.getElementById(id);
+  modal.style.display = "block";
+
+}
+function deleteModal() {
+  modal.style.display = "none";
+}
