688a5ca4a418ecc39b5cc0c946b93ec31b7b2ec8
Mohammed Mogasbe <53066119+MogasbeKelani@users.noreply.github.com>
2020-06-06 10:15:02 -0700

    Add files via upload
    final commit

--- sideNavtoggle.js
+++ sideNavtoggle.js
@@ -1,16 +1,19 @@
 
-var b='<div id="sidenav" class="sidenav">'+
+function clicked(){
+  localStorage.setItem("modal","close");
+}
+var b='<div id="sidenav" class="sidenav sticky">'+
         '<div class="row">'+
   '<h3>Collections</h3>'+
   '</div>'+
   '<div class="row" >'+
-    '<button class="navButton navButtonFirst" onclick="location.href='+"'shopMens.html'"+'"><span>Mens </span></button>'+
+    '<button class="navButton navButtonFirst" onclick="clicked(), location.href='+"'shopMens.html'"+'"><span>Mens </span></button>'+
       '<div class="dropdown">'+
         '<button class="navButton">&#8801</button>'+
           '<div class="dropdown-content">'+
-            '<button class="dropdownElement" onclick="location.href='+"'mensShirts.html'"+'"><span>Shirts </span></button>'+
-            '<button class="dropdownElement"  onclick="location.href='+"'mensPants.html'"+'">Pants</button>'+
-            '<button class="dropdownElement" onclick="location.href='+"'mensAccessories.html'"+'">Accessories</button>'+
+            '<button class="dropdownElement" onclick= "clicked(), location.href='+"'mensShirts.html'"+'"><span>Shirts </span></button>'+
+            '<button class="dropdownElement"  onclick="clicked(), location.href='+"'mensPants.html'"+'">Pants</button>'+
+            '<button class="dropdownElement" onclick="clicked(), location.href='+"'mensAccessories.html'"+'">Accessories</button>'+
           "</div>"+
       "</div>"+
   "</div>"+
@@ -18,26 +21,26 @@ var b='<div id="sidenav" class="sidenav">'+
   "<br>"+
 
   '<div class="row" >'+
-    '<button class="navButton navButtonFirst" onclick="location.href='+"'shopWomens.html'"+'"><span>Womens </span></button>'+
+    '<button class="navButton navButtonFirst" onclick="clicked(), location.href='+"'shopWomens.html'"+'"><span>Womens </span></button>'+
       '<div class="dropdown">'+
         '<button class="navButton">&#8801</button>'+
           '<div class="dropdown-content">'+
-            '<button class="dropdownElement" onclick="location.href='+"'womensShirts.html'"+'"><span>Shirts </span></button>'+
-            '<button class="dropdownElement"  onclick="location.href='+"'womensPants.html'"+'">Pants</button>'+
-            '<button class="dropdownElement" onclick="location.href='+"'womensAccessories.html'"+'">Accessories</button>'+
+            '<button class="dropdownElement" onclick="clicked(), location.href='+"'womensShirts.html'"+'"><span>Shirts </span></button>'+
+            '<button class="dropdownElement"  onclick="clicked(), location.href='+"'womensPants.html'"+'">Pants</button>'+
+            '<button class="dropdownElement" onclick="clicked(), location.href='+"'womensAccessories.html'"+'">Accessories</button>'+
           "</div>"+
       "</div>"+
   "</div>"+
   "<br>"+
   "<br>"+
   '<div class="row" >'+
-    '<button class="navButton navButtonFirst" onclick="location.href='+"'shopChildrens.html'"+'"><span>Childrens </span></button>'+
+    '<button class="navButton navButtonFirst" onclick="clicked(), location.href='+"'shopChildrens.html'"+'"><span>Childrens </span></button>'+
       '<div class="dropdown">'+
         '<button class="navButton">&#8801</button>'+
           '<div class="dropdown-content">'+
-            '<button class="dropdownElement" onclick="location.href='+"'childrensShirts.html'"+'"><span>Shirts </span></button>'+
-            '<button class="dropdownElement"  onclick="location.href='+"'childrensPants.html'"+'">Pants</button>'+
-            '<button class="dropdownElement" onclick="location.href='+"'childrensAccessories.html'"+'">Accessories</button>'+
+            '<button class="dropdownElement" onclick="clicked(), location.href='+"'childrensShirts.html'"+'"><span>Shirts </span></button>'+
+            '<button class="dropdownElement"  onclick="clicked(), location.href='+"'childrensPants.html'"+'">Pants</button>'+
+            '<button class="dropdownElement" onclick="clicked(), location.href='+"'childrensAccessories.html'"+'">Accessories</button>'+
           "</div>"+
       "</div>"+
   "</div>"+
@@ -46,6 +49,21 @@ var b='<div id="sidenav" class="sidenav">'+
 "</div>";
 
 document.getElementById("bar").innerHTML=b;
+var front='<div class="sticky">'+
+  '<div class="container-fluid blueHeader sticky">'+
+    '<h1><span class="white">Shop</span>Kart</h1>'+
+    '<input id="searchBar"class="searchBar" type="text" placeholder="Search..." name="search">'+
+      '<button id="searchButton" class="searchButton" onclick="clicked(), searchItem()">&#x1f50d;</button>'+
+'  </div>'+
+
+  '<div class="change headercolor">'+
+      '<button class="buttonHeader" onclick="clicked(), location.href='+"'index.html'"+'"><span>Home </span></button>'+
+      '<button class="buttonHeader" onclick="clicked(), location.href='+"'mainShop.html'"+'"><span>Shop </span></button>'+
+      '<button class="buttonHeader" onclick="clicked(), location.href='+"'index.html'"+'"><span>Checkout </span></button>'
+'  </div>'+
+
+"</div>";
+document.getElementById("homeHeader").innerHTML=front;
 
 function toggle()
 {
